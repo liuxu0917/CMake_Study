e2d4d68e383c27e138f1e22d5f6a186e83701215
liuxu0917 <liuxu0917@live.com>
2020-10-31 21:57:57 +0800

    更新

---
+++ include/Utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include<iostream>
+
+class Utils{
+
+public:
+    static void OutPut();
+
+};
+
+#endif // UTILS_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include "Utils.h"
 #include "Tools.h"
 
@@ -8,6 +9,18 @@ int main() {
     utils.OutPut();
     Tools tools;
     tools.OutPut();
-    system("pause");
+    std::map<int,std::string> testmap;
+    testmap.insert(std::make_pair(1,"11"));
+    testmap.insert(std::make_pair(2,"22"));
+    testmap.insert(std::make_pair(3,"33"));
+    for (auto iter = testmap.begin(); iter != testmap.end(); ) {
+        std::cout<<iter->second<<std::endl;
+        if( iter->first==1)testmap.erase(iter++);
+        else ++iter;
+    }
+    for ( auto &pair:testmap){
+        std::cout<<pair.second<<std::endl;
+    }
+//    system("pause");
     return 0;
 }
